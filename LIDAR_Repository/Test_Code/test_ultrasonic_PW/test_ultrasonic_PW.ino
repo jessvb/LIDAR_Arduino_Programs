aa15d80783660252e3e3856a822c0309de425057
jessx <jessi0vanb@gmail.com>
2015-07-17 10:02:50 -0700

    Add two ultrasonics & LEDs to test code

--- LIDAR_Repository/Test_Code/test_ultrasonic_PW/test_ultrasonic_PW.ino
+++ LIDAR_Repository/Test_Code/test_ultrasonic_PW/test_ultrasonic_PW.ino
@@ -1,15 +1,43 @@
-int pwPin = 5; // PW input on pin 5
-float distCM;
+int ultraPinRight = 5; // PW input for the right ultrasonic
+int LEDPinRight = 10; // PW output for the right LED
+int ultraPinLeft = 6; // PW input for the left ultrasonic
+int LEDPinLeft = 9; // PW output for the left LED
+#define MAX_DIST 100.0
+#define MIN_DIST 12.7
 
 void setup() {
   Serial.begin(9600);
-  pinMode(pwPin, INPUT);
+  pinMode(ultraPinRight, INPUT);
+  pinMode(ultraPinLeft, INPUT);
+  pinMode(LEDPinRight, OUTPUT);
+  pinMode(LEDPinLeft, OUTPUT);
 }
 
 void loop() {
+  readDistWriteLED (ultraPinRight, LEDPinRight);
+  readDistWriteLED (ultraPinLeft, LEDPinLeft);
+  delay(10);
+}
+
+void readDistWriteLED (int distPin, int ledPin) {
   // ------- READ ULTRASONIC ------- //
-  distCM = (pulseIn(pwPin, HIGH))/147*2.54; // pulse/147*2.54 to cm
-  Serial.print("PW dist (cm): ");
+  // Convert to cm: pulse/147*2.54 = cm
+  float distCM = (pulseIn(distPin, HIGH)) / 147 * 2.54;
+  Serial.print(distPin);
+  Serial.print(" distance (cm): ");
   Serial.println(distCM);
-  delay(100);
+
+  // ------- WRITE LED ------- //
+  if (distCM <= MIN_DIST) {
+    // If very close, light the LED to its maximum
+    analogWrite(ledPin, 255);
+  } else if (distCM >= MAX_DIST) {
+    // If far away, don't light the LED
+    analogWrite(ledPin, 0);
+  } else {
+    // If in between max & min distance, map the light intensity
+    // between 0 and 150 (difficult to see difference between
+    // 150<->255, thus the gradient is better between 0<->150)
+    analogWrite(ledPin, map(distCM, MIN_DIST, MAX_DIST, 150, 0));
+  }
 }
