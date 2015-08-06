d12485a4d25425650f6851252ce8b10ca434976a
Jess <jessi0vanb@gmail.com>
2015-08-06 10:17:26 -0700

    Complete interrupt test code

--- LIDAR_Repository/Test_Code/test_interrupts/test_interrupts.ino
+++ LIDAR_Repository/Test_Code/test_interrupts/test_interrupts.ino
@@ -1,18 +1,16 @@
 /* This code tests using interrupts with the ultrasonics */
 
-#define MICROSECONDS_PER_CM 373.38f
-#define CM_PER_MICROSECONDS (1/MICROSECONDS_PER_CM)
-
-int ultraPinRight = 2; // interrupt input for the right ultrasonic -> int.0
-int LEDPinRight = 10; // PW output for the right LED
-int ultraPinLeft = 3; // interrupt input for the left ultrasonic -> int.1
-int LEDPinLeft = 9; // PW output for the left LED
+const int ultraPinRight = 2; // interrupt input for the right ultrasonic -> int.0
+const int LEDPinRight = 10; // PW output for the right LED
+const int ultraPinLeft = 3; // interrupt input for the left ultrasonic -> int.1
+const int LEDPinLeft = 9; // PW output for the left LED
 
 typedef struct _ultra_state {
   volatile uint32_t startTime; // Start time of pulse
   volatile uint32_t dt; // Pulse duration in microseconds
   volatile boolean pulseStarted; // Flag to ensure correct initial conditions
   volatile boolean lastState; // Keep track of the last logical state of the pin
+  volatile float distance; // Distance reading based on the pulse read
 } ultra_state_t;
 
 ultra_state_t rightUltra;
@@ -26,11 +24,26 @@ void setup() {
 }
 
 void loop() {
+  Serial.println("o hey");
+  delay(1000);
 }
 
 /* When the right ultrasonic starts/finishes a pulse, this method will be called */
 void rightInterrupt() {
+  // If the pulse is high, then save the start time
+  if (digitalRead(ultraPinRight) > 0 && rightUltra.pulseStarted == 0) {
+    rightUltra.startTime = micros();
+    rightUltra.pulseStarted = 1;
+  } else if (rightUltra.pulseStarted) {
+    rightUltra.dt = micros() - rightUltra.startTime;
 
+    // Calculate the distance in centimeters:
+    rightUltra.distance = rightUltra.dt / 147.0 * 2.54;;
+    Serial.println(rightUltra.distance);
+
+    // Clear the flag:
+    rightUltra.pulseStarted = 0;
+  }
 }
 
 /* When the left ultrasonic starts/finishes a pulse, this method will be called */
