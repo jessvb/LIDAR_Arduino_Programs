5428142508f885d6d5aeb20632833666527d1cf7
Jess <jessi0vanb@gmail.com>
2015-08-11 10:22:08 -0700

    Add velocity & pwm test code

--- LIDAR_Repository/Test_Code/test_fuzzy_logic/Structs.h
+++ LIDAR_Repository/Test_Code/test_fuzzy_logic/Structs.h
@@ -7,10 +7,6 @@ typedef const struct _fuzzy_values {
   float medVal; // The midpoint (1 med)
   float hiVal; // The point where med 0.5, hi 0.5
   float maxVal; // The point where hi 1
-
-  //const int LO = 0;
-  //const int MED = 1;
-  //const int HI = 2;
 } fuzzy_values_t;
 
 #endif
--- LIDAR_Repository/Test_Code/test_fuzzy_logic/test_fuzzy_logic.ino
+++ LIDAR_Repository/Test_Code/test_fuzzy_logic/test_fuzzy_logic.ino
@@ -1,8 +1,12 @@
 #include "Structs.h"
 
+#define LO 0
+#define MED 1
+#define HI 2
+
 fuzzy_values_t VEL = {0, 3, 6, 9, 12};
-//fuzzy_values_t DIST = {0, 10, 20, 30, 40};
-//fuzzy_values_t pwm_t _PWM = {0, 63.75, 127.5, 191.25, 255};
+fuzzy_values_t DIST = {0, 10, 20, 30, 40};
+fuzzy_values_t _PWM = {0, 63.75, 127.5, 191.25, 255};
 
 void setup() {
   Serial.begin(9600); //Opens serial connection at 9600bps.
@@ -10,13 +14,35 @@ void setup() {
 
 void loop() {
   float velValues[3]; // [SLOW, MED, FAST]
-  float currVel = 10; // Ten meters/sec
+  float currVel = 10; // in meters/sec
 
   getLoMedHi(currVel, velValues, &VEL);
-
+  
+  Serial.println("Velocity:");
   Serial.print("Lo: "); Serial.println(velValues[0]);
   Serial.print("Med: "); Serial.println(velValues[1]);
   Serial.print("Hi: "); Serial.println(velValues[2]);
+  
+  float distValues[3]; // [SLOW, MED, FAST]
+  float currDist = 2.7; // in meters
+
+  getLoMedHi(currDist, distValues, &DIST);
+
+  Serial.println("Distance:");
+  Serial.print("Lo: "); Serial.println(distValues[0]);
+  Serial.print("Med: "); Serial.println(distValues[1]);
+  Serial.print("Hi: "); Serial.println(distValues[2]);
+  
+  float pwmValues[3]; // [SLOW, MED, FAST]
+  float currPWM = 200; // from 0 to 255
+
+  getLoMedHi(currPWM, pwmValues, &_PWM);
+
+  Serial.println("PWM:");
+  Serial.print("Lo: "); Serial.println(pwmValues[0]);
+  Serial.print("Med: "); Serial.println(pwmValues[1]);
+  Serial.print("Hi: "); Serial.println(pwmValues[2]);
+  
   delay(100000);
 }
 
@@ -27,8 +53,9 @@ void getLoMedHi(float currValue, float* loMedHi, fuzzy_values_t* fuzzyVals) {
   if (currValue >= fuzzyVals->minVal && currValue < fuzzyVals->medVal) {
     // low function -> lo value
     loMedHi[0] = -1 / (fuzzyVals->medVal) * currValue + 1; // y = -1/run*x + 1
-    // left medium function -> med value
-    loMedHi[1] = 1 / (fuzzyVals->medVal) * currValue; // y = 1/run*x
+    // 1 - lo value -> med value
+    // loMedHi[1] = 1 / (fuzzyVals->medVal) * currValue; // y = 1/run*x
+    loMedHi[1] = 1 - loMedHi[0];
     // zero -> hi value
     loMedHi[2] = 0;
   } else if (currValue >= fuzzyVals->medVal && currValue <= fuzzyVals->maxVal) {
@@ -36,8 +63,9 @@ void getLoMedHi(float currValue, float* loMedHi, fuzzy_values_t* fuzzyVals) {
     loMedHi[0] = 0;
     // right medium function -> med value
     loMedHi[1] = -1 / (fuzzyVals->medVal) * currValue + 2; // y = -1/run*x + 2
-    // high function -> hi value
-    loMedHi[2] = 1 / (fuzzyVals->medVal) * currValue - 1; // y = 1/run*x - 1
+    // 1 - med value -> hi value
+    // loMedHi[2] = 1 / (fuzzyVals->medVal) * currValue - 1; // y = 1/run*x - 1
+    loMedHi[2] = 1 - loMedHi[1];
   } else {
     Serial.print("ERROR: "); Serial.print(currValue); Serial.println(" not in range.");
   }
